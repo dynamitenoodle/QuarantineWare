a5d25560fd3b2737fd7b3f13a7fa6c1704ad22da
Coehl <dynamitenoodle@gmail.com>
2020-03-31 01:20:51 -0400

    Working on Washing Hands mini-game

---
+++ Assets/Scripts/WashHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WashHandler : MonoBehaviour
+{
+    public List<List<GameObject>> Sections;
+    int currentSection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+        foreach (Transform child in gameObject.transform.child)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
